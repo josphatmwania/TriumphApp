4fd7741503d984acdfe40dfb510b2427ec048c88
j23747 <josphatmwania237@gmail.com>
2022-09-13 18:27:52 +0300

    Modify Affirmations data class

--- app/src/main/java/com/triumphapp/model/Affirmations.kt
+++ app/src/main/java/com/triumphapp/model/Affirmations.kt
@@ -1,3 +1,15 @@
 package com.triumphapp.model
 
-data class Affirmations()
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmations(
+
+//    Affirmations has one Image & string
+//    The stringResourceId represents an ID for the affirmation text stored in a string resource.
+//    The imageResourceId represents an ID for the affirmation image stored in a drawable resource.
+
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+
+)
