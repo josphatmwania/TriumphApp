c88da19635d3191166a1ca9586246efd1c9f6988
j23747 <josphatmwania237@gmail.com>
2022-09-14 11:01:30 +0300

    Add Affirmation methods

--- app/src/main/java/com/triumphapp/MainActivity.kt
+++ app/src/main/java/com/triumphapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.triumphapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.triumphapp.model.Affirmations
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
+
+    @Composable
+    fun AffirmationApp() {
+        val context = LocalContext.current
+        AffirmationTheme {
+
+        }
+    }
+
+
+    @Composable
+    fun AffirmationCard(affirmation: Affirmations){
+
+
+    }
+
+
+
+
+
+
--- app/src/main/java/com/triumphapp/data/Datasource.kt
+++ app/src/main/java/com/triumphapp/data/Datasource.kt
@@ -5,12 +5,12 @@ import com.triumphapp.model.Affirmations
 class Datasource() {
 
 //    Loading list of affirmations
-    fun loadAffirmations() : List<Affirmations> {
-        return listOf<Affirmations>(
+    fun loadAffirmations() : List<Affirmation> {
+        return listOf<Affirmation>(
             Affirmation(R.string.affirmation1, R.drawable.image1),
-            Affirmation(R.string.affirmation1, R.drawable.image2),
-            Affirmation(R.string.affirmation1, R.drawable.image3),
-            Affirmation(R.string.affirmation1, R.drawable.image4),
+            Affirmation(R.string.affirmation2, R.drawable.image2),
+            Affirmation(R.string.affirmation3, R.drawable.image3),
+            Affirmation(R.string.affirmation4, R.drawable.image4),
             )
     }
 
