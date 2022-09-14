e95de37c54448e95d37bcbc142fdf9281c0f8a06
j23747 <josphatmwania237@gmail.com>
2022-09-14 11:17:46 +0300

    Add Affirmationcard funtion

--- app/src/main/java/com/triumphapp/MainActivity.kt
+++ app/src/main/java/com/triumphapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.triumphapp
 
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.triumphapp.model.Affirmations
+import java.lang.reflect.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,7 +25,22 @@ class MainActivity : AppCompatActivity() {
 
 
     @Composable
-    fun AffirmationCard(affirmation: Affirmations){
+    fun AffirmationCard(affirmation: Affirmation, modifier : Modifier = Modifier) {
+        Card(modifier = modifier.padding(8.dp), elevation = 4.dp) {
+            Column{
+                Image(
+                    painter = painterResource(affirmation.imageResourceId),
+                    contentDescription = stringResource(affirmation.stringResourceId),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(194.dp),
+                    contentScale = ContentScale.crop
+                )
+            }
+
+
+        }
+
 
 
     }
