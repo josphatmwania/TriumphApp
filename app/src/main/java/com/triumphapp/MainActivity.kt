6e5cb9e6c99e96e7506a3711c86ad676310c11a7
j23747 <josphatmwania237@gmail.com>
2022-09-15 10:50:08 +0300

    Detele affirmation code

--- app/src/main/java/com/triumphapp/MainActivity.kt
+++ app/src/main/java/com/triumphapp/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.triumphapp
 
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.triumphapp.model.Affirmations
-import java.lang.reflect.Modifier
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,36 +12,6 @@ class MainActivity : AppCompatActivity() {
 
 
 }
-
-    @Composable
-    fun AffirmationApp() {
-        val context = LocalContext.current
-        AffirmationTheme {
-
-        }
-    }
-
-
-    @Composable
-    fun AffirmationCard(affirmation: Affirmation, modifier : Modifier = Modifier) {
-        Card(modifier = modifier.padding(8.dp), elevation = 4.dp) {
-            Column{
-                Image(
-                    painter = painterResource(affirmation.imageResourceId),
-                    contentDescription = stringResource(affirmation.stringResourceId),
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(194.dp),
-                    contentScale = ContentScale.crop
-                )
-            }
-
-
-        }
-
-
-
-    }
 
 
 
