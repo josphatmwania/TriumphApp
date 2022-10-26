6d49c54529327ff88bdcee4e5fb796c666e3ae2e
j23747 <josphatmwania237@gmail.com>
2022-10-26 10:32:29 +0300

    Add App Module

--- app/src/main/java/com/triumphapp/di/AppModule.kt
+++ app/src/main/java/com/triumphapp/di/AppModule.kt
@@ -1,9 +1,24 @@
 package com.triumphapp.di
 
 import dagger.Module
+import dagger.Provides
+import dagger.hilt.components.SingletonComponent
+import dagger.hilt.InstallIn
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 
+/**
+ * Providing the coroutines dispatcher
+ *
+ */
 
 
+@Module
+@InstallIn(SingletonComponent::class)
 class AppModule {
+    @Provides
+    @Singleton
+    fun provideDispatcher() : CoroutineDispatcher = Dispatchers.IO
 }
