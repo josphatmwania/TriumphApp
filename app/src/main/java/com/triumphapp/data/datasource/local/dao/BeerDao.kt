8c9a0d811ca2a0340d925cb8d8706095de8f6477
j23747 <josphatmwania237@gmail.com>
2022-10-27 07:45:43 +0300

    Add Beer get fun and return a list of beers

--- app/src/main/java/com/triumphapp/data/datasource/local/dao/BeerDao.kt
+++ app/src/main/java/com/triumphapp/data/datasource/local/dao/BeerDao.kt
@@ -1,4 +1,27 @@
 package com.triumphapp.data.datasource.local.dao
 
-class BeerDao {
+import androidx.room.*
+import com.triumphapp.data.datasource.local.models.BeerEntity
+
+/**
+ * Defining the Database interactions
+ */
+
+
+@Dao
+interface BeerDao {
+
+    /**
+     * Getting all the beers from the DB
+     * @return a list of all the beers in the DB
+     */
+
+
+    /**
+     * Returns List in ascending order
+     */
+    @Query("SELECT * FROM beers WHERE name LIKE '%' || :filterByName || '%' ORDER BY name ASC")
+    suspend fun  getAllBeers(filterByName: String = ""): List<BeerEntity>
+
+
 }
